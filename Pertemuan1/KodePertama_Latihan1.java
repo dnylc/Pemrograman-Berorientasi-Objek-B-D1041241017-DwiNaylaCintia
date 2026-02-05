6aa0d48f9d68a7de04d08023a20367baaa927b70
DwiNaylaCintia <d1041241017@student.untan.ac.id>
2026-02-05 15:32:10 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama_Latihan1.java
@@ -0,0 +1,11 @@
+public class KodePertama_Latihan1 {
+public static void main (String[] args) {
+        System.out.println("Nama : Dwi Nayla Cintia");
+        System.out.println("NIM : D1041241017");
+        System.out.println("Semester : 4");
+        System.out.println("Prodi : Informatika");
+    }
+}
+// Nama : Dwi Nayla Cintia
+// NIM : D1041241017
+// Tanggal : 5 Februari 2026
