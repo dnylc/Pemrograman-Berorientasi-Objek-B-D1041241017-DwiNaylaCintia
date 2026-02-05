5d8a4206c5f3f01104a1824d94d0fef1740c4a01
DwiNaylaCintia <d1041241017@student.untan.ac.id>
2026-02-05 15:29:33 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,10 @@
+public class KodePertama {
+public static void main (String[] args) {
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya.");
+    }
+
+}
+// Nama : Dwi Nayla Cintia
+// NIM : D1041241017
+// Tanggal : 5 Februari 2026
