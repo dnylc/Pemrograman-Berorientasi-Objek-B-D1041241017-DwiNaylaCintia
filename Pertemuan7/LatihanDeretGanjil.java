c219f9116ed2ef503e8dd25710c020fb03ef1ee5
DwiNaylaCintia <d1041241017@student.untan.ac.id>
2026-02-26 23:41:12 +0700

    Menambahkan Latihan 1 Pertemuan 7

---
+++ Pertemuan7/LatihanDeretGanjil.java
@@ -0,0 +1,20 @@
+package Pertemuan7;
+
+public class LatihanDeretGanjil {
+     public static void main(String[] args){
+        int angka = 1; // Inisialisasi angka dengan nilai awal 1
+        System.out.println("=== Bilangan Ganjil 1-15 ===");
+
+        // Menggunakan while loop untuk mencetak bilangan ganjil dari 1 hingga 15
+        while (angka <= 15) {
+            System.out.print(angka + " "); // Mencetak angka ganjil saat ini diikuti oleh spasi
+
+            angka += 2; // Menambahkan 2 untuk mendapatkan bilangan ganjil berikutnya
+        } System.out.println();
+    }
+
+}
+
+// Nama : Dwi Nayla Cintia
+// NIM : D1041241017
+// Kelas : B
