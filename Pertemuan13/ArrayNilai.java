c3bda97e9cef8a67b0078b849254b1117dab2c69
DwiNaylaCintia <d1041241017@student.untan.ac.id>
2026-05-07 17:36:58 +0700

    Menambahkan Latihan 1 Pertemuan 13

---
+++ Pertemuan13/ArrayNilai.java
@@ -0,0 +1,20 @@
+package Pertemuan13;
+
+public class ArrayNilai {
+    public static void main(String[] args) {
+        int[] nilai = {85, 90, 78, 92, 88};
+
+        System.out.println("=== DAFTAR NILAI ===");
+
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Nilai " + (i + 1) + ": " + nilai[i]);
+        }
+        System.out.println ("\nTotal Nilai : " + nilai.length);
+    }
+}
+/*  
+Nama : Dwi Nayla Cintia
+NIM : D1041241017
+Kelas : B
+*/    
+
