c76314b73fd382d8559f85d28b02ecf51478f482
DwiNaylaCintia <d1041241017@student.untan.ac.id>
2026-05-07 17:37:19 +0700

    Menambahkan Latihan 2 Perrtemuan 13

---
+++ Pertemuan13/ArrayNama.java
@@ -0,0 +1,18 @@
+package Pertemuan13;
+
+public class ArrayNama {
+    public static void main(String[] args) {
+        String[] nama = {"Andi", "Budi", "Citra", "Deni", "Eka"};
+
+        System.out.println("=== DAFTAR NAMA ===");
+
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println((i + 1) + ". " + nama[i]);
+        }
+    }
+}
+/*  
+Nama : Dwi Nayla Cintia
+NIM : D1041241017
+Kelas : B
+*/ 
